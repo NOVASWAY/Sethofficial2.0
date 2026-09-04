18ddd07ad9672c6ce2cad414b638e623d0670621
NOVASWAY <njauwangari6@gmail.com>
2026-09-04 23:40:40 +0300

    fix: wrap app with LanguageProvider for useLanguage hook

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,22 +4,25 @@ import type React from "react"
 import { SessionProvider } from "next-auth/react"
 import { AuthProvider } from "@/contexts/auth-context"
 import { ThemeProvider } from "next-themes"
+import { LanguageProvider } from "@/contexts/language-context"
 import { Toaster } from "sonner"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <AuthProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
-          themes={["light", "dark", "clinic"]}
-          storageKey="clinic-theme"
-        >
-          {children}
-          <Toaster position="top-right" richColors />
-        </ThemeProvider>
+        <LanguageProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+            themes={["light", "dark", "clinic"]}
+            storageKey="clinic-theme"
+          >
+            {children}
+            <Toaster position="top-right" richColors />
+          </ThemeProvider>
+        </LanguageProvider>
       </AuthProvider>
     </SessionProvider>
   )
